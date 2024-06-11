5268382cb91eb8fad8a86fd681e3b82a00add08d
markoscomks <markoscomks@gmail.com>
2024-06-11 12:57:46 +0100

    Added header for so_long

---
+++ sources/so_long.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 12:52:30 by marsoare          #+#    #+#             */
+/*   Updated: 2024/06/11 12:54:55 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "../libraries/minilibx-linux/mlx.h"
+# include "../libraries/libft/libft.h"
+# include <fcntl.h>
+
+#endif
