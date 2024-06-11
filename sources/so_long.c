f7660482816a649abf1386ade15a67ebe70eaa04
markoscomks <markoscomks@gmail.com>
2024-06-11 15:03:30 +0100

    Basic setup for so_long - window showering on screen

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,18 +6,37 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:50:16 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/11 12:52:23 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/11 15:01:47 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <stdio.h>
+
+typedef struct	s_game {
+	void	*connection;
+	void	*window;
+}				t_game;
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_game	game;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 480, 320, "Cursus Strike!");
-	mlx_loop(mlx);
+	game.connection = mlx_init();
+	game.window = mlx_new_window(game.connection, 640, 480, "Hello world!");
+	if (!game.window)
+	{
+		mlx_destroy_display(game.connection);
+		free(game.connection);
+		free(game.window);
+		return (1);
+	}
+	mlx_loop(game.connection);
+	mlx_destroy_display(game.connection);
+	mlx_destroy_window(game.connection, game.window);
+	free(game.connection);
+	free(game.window);
+	return(0);
 }
+
+
