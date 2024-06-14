9996a950494140af1847532fd9e79a3bb469e386
markoscomks <markoscomks@gmail.com>
2024-06-14 15:39:32 +0100

    Added X11/keysym lib for keyboard code macros

--- sources/so_long.h
+++ sources/so_long.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:52:30 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/11 12:54:55 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/14 15:36:46 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # include "../libraries/minilibx-linux/mlx.h"
 # include "../libraries/libft/libft.h"
 # include <fcntl.h>
+# include <X11/keysym.h>
 
 #endif
