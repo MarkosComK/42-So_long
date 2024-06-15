cbad19bc520982347438c4e353f72d3e42b259e3
markoscomks <markoscomks@gmail.com>
2024-06-15 21:12:50 +0100

    Changed func to + path

--- sources/so_long.h
+++ sources/so_long.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:52:30 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/15 19:58:01 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/15 21:08:46 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ typedef struct	s_map
 	int		columns;
 }				t_map;
 
-void	game_start(t_game *game);
+void	game_start(char *path, t_game *game);
 int		game_over(int keycode, t_game *game);
 
 #endif
