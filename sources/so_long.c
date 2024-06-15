87a964556a05a821c5722740242ca18844a0e091
markoscomks <markoscomks@gmail.com>
2024-06-15 21:12:33 +0100

    Added path param to funcs - using game_over func

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,28 +6,14 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:50:16 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/15 19:55:28 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/15 21:10:20 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-int	endgame(int keycode, t_game *game)
-{
-	if (keycode == XK_Escape || keycode == XK_q)
-	{
-		mlx_destroy_window(game->mlx, game->win);
-		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-		exit(0);
-	}
-	return (1);
-}
-
-
-
-int	main(void)
+int	main(int ac, char **av)
 {
 	t_game	game;
 	void	*img;
@@ -38,12 +24,14 @@ int	main(void)
 
 	w = &wi;
 	h = &he;
-	game_start(&game);
-	img	= mlx_xpm_file_to_image(game.mlx, "./assets/character/chort_run_anim_f1.xpm", w,  h);
-	mlx_put_image_to_window(game.mlx, game.win, img, 50, 50);
-	mlx_key_hook(game.win, endgame, &game);
-	//game_over(&game);
-	mlx_destroy_image(game.mlx, img);
-	mlx_loop(game.mlx);
+	if (ac == 2)
+	{
+		game_start(av[1], &game);
+		img	= mlx_xpm_file_to_image(game.mlx, "./assets/character/chort_run_anim_f1.xpm", w,  h);
+		mlx_put_image_to_window(game.mlx, game.win, img, 50, 50);
+		mlx_key_hook(game.win, game_over, &game);
+		mlx_destroy_image(game.mlx, img);
+		mlx_loop(game.mlx);
+	}
 }
 
