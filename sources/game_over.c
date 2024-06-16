2d60e1282f8a7280057ace85bcd98cfb138922bf
markoscomks <markoscomks@gmail.com>
2024-06-16 19:52:19 +0100

    Added new func to clean everyting

---
+++ sources/game_over.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_over.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 18:46:40 by marsoare          #+#    #+#             */
+/*   Updated: 2024/06/16 18:46:41 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void  game_over(t_game *game)
+{
+  int i;
+
+  i = 0;
+  while (game->map.map[i])
+    free(game->map.map[i++]);
+  mlx_destroy_image(game->mlx, game->map.img[0]);
+  mlx_destroy_image(game->mlx, game->map.img[1]);
+  mlx_destroy_window(game->mlx, game->win);
+  mlx_destroy_display(game->mlx);
+  free(game->map.img);
+  free(game->map.map);
+  free(game->mlx);
+  exit(0);
+}
