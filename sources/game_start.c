17bda730210dda7ca7a7308c50dd53552f5e38b3
MarkosComK <markoscomks@gmail.com>
2024-06-16 02:43:15 +0100

    Changed game struct - removed drawmap func

--- sources/game_start.c
+++ sources/game_start.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 23:38:19 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/15 22:10:31 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/16 02:30:55 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,96 +32,42 @@ int		count_lines(char *path)
 	return (count);
 }
 
-void	init_map(t_map *map)
+void	init_map(t_game *game)
 {
-	map->map = NULL;
-	map->img = NULL;
-	map->columns = 0;
-	map->lines = 0;
-	map->width = 0;
-	map->height = 0;
+	game->map.map = NULL;
+	game->map.img = NULL;
+	game->map.columns = 0;
+	game->map.lines = 0;
+	game->map.width = 0;
+	game->map.height = 0;
 }
 
-void	set_map(char *path, t_map *map)
+void	set_map(char *path, t_game *game)
 {
 	int		fd;
 	int		i;
 	char	*str;
 
-	init_map(map);
+	init_map(game);
 	fd = open(path, O_RDONLY);
-	map->lines = count_lines(path);
-	map->map = malloc(sizeof(char **) * map->lines);
+	game->map.lines = count_lines(path);
+	game->map.map = malloc(sizeof(char **) * game->map.lines);
 	i = 0;
 	while ((str = get_next_line(fd))) 
 	{
-		map->map[i] = malloc(sizeof(char *) * ft_strlen(str));
-		map->map[i] = ft_strdup(str);
+		game->map.map[i] = malloc(sizeof(char *) * ft_strlen(str));
+		game->map.map[i] = ft_strdup(str);
 		free(str);
 		i++;
 	}
-	map->width = 16 * ft_strlen(map->map[0]);
-	map->height = 16 * map->lines;
-	map->columns = ft_strlen(map->map[0]);
+	game->map.columns = ft_strlen(game->map.map[0]);
 	close(fd);
-}
-
-void	draw_map(t_game game, t_map *map)
-{
-	t_img	img;
-	int	i;
-	int	j;
-	int *w, *h;
-	int wi, he;
-	wi = 16;
-	he = 20;
-	w = &wi;
-	h = &he;
-
-	i = -1;
-	img.img = mlx_xpm_file_to_image(game.mlx, "./assets/chort_run_anim_f1.xpm", w, h);
-	while (map->map[i])
-	{
-		j = i + 1;
-		while (map->map[j])
-		{
-			mlx_put_image_to_window(game.mlx, game.win, img.img, 16*i, 16*j);
-			j++;
-		}
-		i++;
-	}
 }
 
 void	game_start(char *path, t_game *game)
 {
-	t_map	map;
-	//map = malloc(sizeof(t_map));
-	set_map(path, &map);
+	set_map(path, game);
 	game->mlx = mlx_init();
-	ft_printf("%s", map.map[0]);
-	ft_printf("%i", map.width);
-	t_img	img;
-	int	i;
-	int	j;
-	int *w, *h;
-	int wi, he;
-	wi = 16;
-	he = 20;
-	w = &wi;
-	h = &he;
-
-	i = -1;
-	img.img = mlx_xpm_file_to_image(game->mlx, "./assets/character/chort_run_anim_f1.xpm", w, h);
-	while (map.map[i])
-	{
-		j = i + 1;
-		while (map.map[j])
-		{
-			mlx_put_image_to_window(game->mlx, game->win, img.img, 16*i, 16*j);
-			j++;
-		}
-		i++;
-	}
-	game->win = mlx_new_window(game->mlx, map.width, map.height, "so_long");
+	game->win = mlx_new_window(game->mlx, 300, 400, "so_long");
 }
 
