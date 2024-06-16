af7cc37314294e4df99be69d9406f536d3c39a8a
markoscomks <markoscomks@gmail.com>
2024-06-16 14:13:09 +0100

    changed game_over to handle_key

---
+++ sources/handle_key.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_over.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 19:41:53 by marsoare          #+#    #+#             */
+/*   Updated: 2024/06/16 14:09:59 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int		handle_key(int keycode, t_game *game)
+{
+	if (keycode == XK_Escape)
+	{
+		mlx_destroy_window(game->mlx, game->win);
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		exit(0);
+	}
+	return (1);
+}
