0b190393c66b4cc0f9923093c4a6f53bcbcd30ba
markoscomks <markoscomks@gmail.com>
2024-06-22 17:08:03 +0100

    Handling new status for move the player

--- sources/handle_key.c
+++ sources/handle_key.c
@@ -6,33 +6,48 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 19:41:53 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/16 14:54:18 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/22 17:02:31 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	restart_direction(t_game *game)
+{
+	game->player.move.w = 0;
+	game->player.move.a = 0;
+	game->player.move.s = 0;
+	game->player.move.d = 0;
+}
+
 int handle_key(int keycode, t_game *game)
 {
-  if (keycode == XK_r)
-    game->start = 1;
-  if (keycode == XK_w)
-  {
-    game->player_y--;
-  }
-  if (keycode == XK_a)
-  {
-    game->player_x--;
-  }
-  if (keycode == XK_d)
-  {
-    game->player_x++;
-  }
-  if (keycode == XK_s)
-  {
-    game->player_y++;
-  }
-  if (keycode == XK_Escape)
-    game_over(game);
-  return (1);
+	restart_direction(game);
+	if (keycode == XK_r)
+	{
+		game->start = 1;
+	}
+	if (keycode == XK_w)
+	{
+		game->player.pos_y--;
+		game->player.move.w = 1;
+	}
+	if (keycode == XK_a)
+	{
+		game->player.pos_x--;
+		game->player.move.a = 1;
+	}
+	if (keycode == XK_s)
+	{
+		game->player.pos_y++;
+		game->player.move.s = 1;
+	}
+	if (keycode == XK_d)
+	{
+		game->player.pos_x++;
+		game->player.move.d = 1;
+	}
+	if (keycode == XK_Escape)
+		game_over(game);
+	return (1);
 }
