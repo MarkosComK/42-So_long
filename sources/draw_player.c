e10591595aa61d056f05c13347dc87e7aef4c482
markoscomks <markoscomks@gmail.com>
2024-06-22 17:11:20 +0100

    Player moving in all directions

--- sources/draw_player.c
+++ sources/draw_player.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 14:50:48 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/22 17:02:10 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/22 17:10:09 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@ int	update_player(t_game *game)
 	draw_map(game);
 	mlx_put_image_to_window(game->mlx, game->win, game->map.img[1], 
 			2*game->player.pos_x, 2*game->player.pos_y);
+	if (game->player.move.w)
+		game->player.pos_y--;
+	if (game->player.move.a)
+		game->player.pos_x--;
+	if (game->player.move.s)
+		game->player.pos_y++;
 	if (game->player.move.d)
 		game->player.pos_x++;
 	ft_printf("%i\n", game->player.start_x);
