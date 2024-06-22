d7af2a79532087cec981a0c190bd08a369984d8e
markoscomks <markoscomks@gmail.com>
2024-06-22 17:07:08 +0100

    Added variable to check player moving

--- sources/draw_player.c
+++ sources/draw_player.c
@@ -6,15 +6,31 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 14:50:48 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/16 14:53:54 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/22 17:02:10 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	draw_player(t_game *game)
+int	start_player(t_game *game)
 {
-	ft_printf("its working!");
-	mlx_put_image_to_window(game->mlx, game->win, game->map.img[1], S*game->player_x, S*game->player_y);
+	mlx_put_image_to_window(game->mlx, game->win, game->map.img[1], 
+			S*game->player.start_x, S*game->player.start_y);
+	return (0);
+}
+
+int	update_player(t_game *game)
+{
+	clean_images(game);
+	draw_map(game);
+	mlx_put_image_to_window(game->mlx, game->win, game->map.img[1], 
+			2*game->player.pos_x, 2*game->player.pos_y);
+	if (game->player.move.d)
+		game->player.pos_x++;
+	ft_printf("%i\n", game->player.start_x);
+	ft_printf("%i\n", game->player.start_y);
+	ft_printf("%i\n", game->player.pos_x);
+	ft_printf("%i\n", game->player.pos_y);
+	ft_printf("ENDS HERE\n");
 	return (0);
 }
