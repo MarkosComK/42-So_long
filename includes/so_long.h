85a27fdc5e52f887734931b1bb5ac125c6c41807
markoscomks <markoscomks@gmail.com>
2024-06-23 20:05:30 +0100

    Added missing funcs to file

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 18:27:40 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/23 19:26:07 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/23 19:59:46 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include "./colors.h"
 # include "../libraries/libft/libft.h"
 # include <stdlib.h>
+# include <fcntl.h>
 # include <stdbool.h>
 # include <X11/keysym.h>
 
@@ -22,6 +23,23 @@
 #define true 1
 #define false 0
 
+typedef struct  s_map
+{
+    char    **str;
+    int     rows;
+    int     cols;
+}               t_map;
+
+typedef struct  s_game
+{
+    t_map   map;
+}               t_game;
+
 BOOL	valid_ber(char *path);
+void	start_game(char *map_name);
+void    set_map(t_game *game, char *map_path);
+
+// UTILITIES TO DELETE LATER
+void	print_map(t_game *game);
 
 #endif
