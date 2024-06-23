36ab5ba650b7873490a2c358eb731a3fe3ded850
markoscomks <markoscomks@gmail.com>
2024-06-23 18:46:29 +0100

    Added basic includes

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,13 +6,15 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 18:27:40 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/23 18:28:04 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/23 18:43:37 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-
-
+# include "./colors.h"
+# include <stdlib.h>
+# include <stdbool.h>
+# include <X11/keysym.h>
 
 #endif
