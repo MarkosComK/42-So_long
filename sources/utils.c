488b9876c2a37f3e7519ec91ef87f5e11f8e19b8
markoscomks <markoscomks@gmail.com>
2024-06-23 20:39:33 +0100

    moduled error msg

--- sources/utils.c
+++ sources/utils.c
@@ -6,11 +6,18 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 19:58:32 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/23 19:59:06 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/23 20:18:05 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+int	error_msg(char *msg)
+{
+	ft_printf("ERROR\n%s%s%s\n", RED, msg, DEFAULT);
+	exit(0);
+	return (0);
+}
 
 void	print_map(t_game *game)
 {
