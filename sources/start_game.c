c4fa58da382d617a33c32718aabb2a25e5e9a895
markoscomks <markoscomks@gmail.com>
2024-06-26 14:19:41 +0100

    Trying to load image on screen

--- sources/start_game.c
+++ sources/start_game.c
@@ -6,15 +6,19 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 19:30:49 by marsoare          #+#    #+#             */
-/*   Updated: 2024/06/25 15:05:37 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/06/25 15:05:37by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 void	start_game(t_game *game)
 {
+	t_img	sprite;
+
 	game->mlx = mlx_init();
 	game->win= mlx_new_window(game->mlx, game->map.width, game->map.height + SIZE, "So Long");
+	sprite = new_file_img("../sprites/pac-man/black.xpm", game);
+	mlx_put_image_to_window(sprite.win.mlx, sprite.win.win, sprite.ptr, 0, 0);
 	mlx_key_hook(game->win, key_press, game);
 	mlx_loop(game->mlx);
 }
