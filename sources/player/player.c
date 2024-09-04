07c5dd9c6fb1b672a57d5d582ac144b13a9d3e22
markoscomks <markoscomks@gmail.com>
2024-09-04 17:46:18 +0100

    Player init missing define sprite

--- sources/player/player.c
+++ sources/player/player.c
@@ -15,8 +15,25 @@
 
 void	init_player(t_game *game)
 {
+	t_pos	s_pos;
+	t_pos	d_pos;
+	t_entity	player;
+
+	player = game->player;
 	set_player_pos(game);
-	ft_printf("Player x: %i y: %i\n", game->player.s_pos.x, game->player.s_pos.y);
+	s_pos = (t_pos){player.s_pos.x, player.s_pos.x}; //del later
+	player.d_pos = (t_pos){0, 0};
+	player.current_sprite = 0; //load sprite here
+	d_pos = player.d_pos; //del later
+	
+	
+	ft_printf("dest_pos x: %i y: %i\n", d_pos.x, d_pos.y);
+}
+
+void	load_player_sprite(t_game *game)
+{
+	(void) game;
+
 }
 
 void	set_player_pos(t_game *game)
