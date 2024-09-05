b92688bcf7c6c607fefe79c836cf5e9c18111bf6
markoscomks <markoscomks@gmail.com>
2024-09-05 11:08:08 +0100

    Fixed norm

--- sources/start_map.c
+++ sources/start_map.c
@@ -6,35 +6,11 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 19:33:16 by marsoare          #+#    #+#             */
-/*   Updated: 2024/09/03 20:59:34 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/09/05 11:07:57 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
-void	put_map()
-{
-
-}
-
-int		count_lines(char *map_path)
-{
-	int		count;
-	char	*str;
-	int		fd;
-
-	count = 0;
-	fd = open(map_path, O_RDONLY);
-	str = get_next_line(fd);
-	while (str)
-	{
-		count++;
-		free(str);
-		str = get_next_line(fd);
-	}
-	close(fd);
-	return (count);
-}
 
 void	set_map_tiles(t_game *game)
 {
@@ -43,18 +19,20 @@ void	set_map_tiles(t_game *game)
 	i = 0;
 	game->map.tiles = (t_tile **)ft_calloc(game->map.rows + 1, sizeof(t_tile *)
 			* SZ);
-	/* to do 
 	if (!game->map.tiles)
-		tiles_error(0, game);
-	*/
+	{
+		ft_putendl_fd("Tiles alocation failes", 2);
+		exit (1);
+	}
 	while (i < game->map.rows)
 	{
 		game->map.tiles[i] = (t_tile *)ft_calloc(sizeof(t_tile *) * SZ,
-			ft_strlen(game->map.data[i]) + 1);
-		/* to do
+				ft_strlen(game->map.data[i]) + 1);
 		if (!game->map.tiles[i++])
-			tiles_error(0, game);
-		*/
+		{
+			ft_putendl_fd("Tiles alocation failes", 2);
+			exit (1);
+		}
 		i++;
 	}
 }
@@ -87,7 +65,6 @@ void	fill_map(t_game *game)
 		j = 0;
 		while (mapd[i][j])
 		{
-			//check_type(game, mapd[i], x, y);
 			game->map.tiles[y][x] = new_tile(mapd[i][j], x * SZ, y * SZ);
 			game->map.tiles[y][x].sprite_path = NULL;
 			x++;
@@ -103,22 +80,21 @@ void	start_world(t_game *game)
 	t_img	*world;
 
 	world = (t_img *)ft_calloc(1, sizeof(t_img));
-	world->ptr = mlx_new_image(game->mlx, game->map.cols * SZ, game->map.rows * SZ);
-	/*
+	world->ptr = mlx_new_image(game->mlx, game->map.cols * SZ,
+			game->map.rows * SZ);
 	if (!world->ptr)
 	{
-		free later
-		return ;
+		ft_putendl_fd("Failed to alloc world", 2);
+		exit (1);
 	}
-	*/
 	world->addr = mlx_get_data_addr(world->ptr, &world->bpp,
-		  &world->line_len, &world->endian);
+			&world->line_len, &world->endian);
 	world->w = game->map.cols * SZ;
 	world->h = game->map.rows * SZ;
 	game->world = world;
 }
 
-void    set_map(t_game *game, char *map_path)
+void	set_map(t_game *game, char *map_path)
 {
 	int		fd;
 	int		i;
@@ -128,12 +104,14 @@ void    set_map(t_game *game, char *map_path)
 	game->map.rows = count_lines(map_path);
 	game->map.data = ft_calloc(sizeof(char **), game->map.rows + 1);
 	i = 0;
-	while ((str = get_next_line(fd))) 
+	str = get_next_line(fd);
+	while (str)
 	{
 		game->map.data[i] = ft_calloc(sizeof(char *), ft_strlen(str) + 1);
 		ft_strlcpy(game->map.data[i], str, ft_strlen(str));
 		free(str);
 		i++;
+		str = get_next_line(fd);
 	}
 	close(fd);
 	set_map_tiles(game);
